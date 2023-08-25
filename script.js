5b9565e0dc40a7d2695fedbb5d8496f0a2e5fa1c
gap14360 <67998380+gap14360@users.noreply.github.com>
2023-08-25 19:29:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+/** Animation for Image-1 **/
+gsap.from("#img-1", {
+    delay: 0.6,
+    opacity: 0,
+    duration: 1,
+    y: 60
+})
+
+/** Animation for Image-2 **/
+gsap.from("#img-2", {
+    delay: 0.6,
+    opacity: 0,
+    duration: 1,
+    x: 80
+})
+
+/** Animation for Image-3 **/
+gsap.from("#img-3", {
+    delay: 0.6,
+    opacity: 0,
+    duration: 1,
+    y: -80
+})
+
+gsap.from("#main>h1", {
+    delay: 0.4,
+    opacity: 0,
+    duration: 1,
+})
